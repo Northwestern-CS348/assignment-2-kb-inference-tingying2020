05f5f4b418c33c4575a96ebfd1b1126862b87b41
tingying2020 <479726658@qq.com>
2019-02-04 22:43:10 -0600

    assignment2_TingYing

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ class KBTest(unittest.TestCase):
 
     def setUp(self):
         # Assert starter facts
-        file = 'statements_kb4.txt'
+        file = 'statements_kb5.txt'
         self.data = read.read_tokenize(file)
         data = read.read_tokenize(file)
         self.KB = KnowledgeBase([], [])
         for item in data:
             if isinstance(item, Fact) or isinstance(item, Rule):
                 self.KB.kb_assert(item)
-        
+    '''
     def test1(self):
         # Did the student code contain syntax errors, AttributeError, etc.
         ask1 = read.parse_input("fact: (motherof ada ?X)")
@@ -70,7 +70,102 @@ class KBTest(unittest.TestCase):
         print(' Asking if', ask1)
         answer = self.KB.kb_ask(ask1)
         self.assertEqual(str(answer[0]), "?X : bing")
+    '''
+    def test6(self):
+        """this student generated test ensures retract only removes facts and rules that are supported by
+        1 or less fact-rule pairs
+        """
+        r1 = read.parse_input("fact: (dresslike profHammond TonyStark)")
+        print(' Retracting', r1)
+        self.KB.kb_retract(r1)
+        ask1 = read.parse_input("fact: (isliterally ?X TonyStark)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertEqual(str(answer[0]), "?X : profHammond")
+        ask2 = read.parse_input("fact: (resembles profHammond ?Y)")
+        print(' Asking if', ask2)
+        answer = self.KB.kb_ask(ask2)
+        self.assertFalse(answer)
 
+    def test7(self):
+        """this student generated test ensures retracting the 2nd fact in the lhs of a rule
+        successfully retracts the final inferred fact but re-assertion re-infers the fact
+        """
+        r1 = read.parse_input("fact: (lookslike profHammond TonyStark)")
+        print(' Retracting', r1)
+        self.KB.kb_retract(r1)
+        ask1 = read.parse_input("fact: (resembles profHammond ?Y)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertFalse(answer)
+        a1 = read.parse_input("fact: (lookslike profHammond TonyStark)")
+        print(' Reasserting', a1)
+        self.KB.kb_assert(a1)
+        ask2 = read.parse_input("fact: (resembles profHammond ?Y)")
+        print(' Asking if', ask2)
+        answer = self.KB.kb_ask(ask2)
+        self.assertEqual(str(answer[0]), "?Y : TonyStark")
+
+    def test8(self):
+        """this student generated test ensures retracting a fact that supports inferences 2 links away
+        also retracts all facts down that chain
+        """
+        r1 = read.parse_input("fact: (techgenius profHammond)")
+        print(' Retracting', r1)
+        self.KB.kb_retract(r1)
+        r2 = read.parse_input("fact: (talkslike profHammond TonyStark)")
+        print(' Retracting', r2)
+        self.KB.kb_retract(r2)
+        ask1 = read.parse_input("fact: (isliterally ?X TonyStark)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertFalse(answer)
+        ask2 = read.parse_input("fact: (IronMan ?X)")
+        print(' Asking if', ask2)
+        answer = self.KB.kb_ask(ask2)
+        self.assertFalse(answer)
+        ask3 = read.parse_input("fact: (Avenger ?X)")
+        print(' Asking if', ask3)
+        answer = self.KB.kb_ask(ask3)
+        self.assertFalse(answer)
+
+    def test9(self):
+        """this student generated test ensures retracting a fact that supports two or more facts or rules
+        successfully retracts all inferred facts and rules
+        """
+        r1 = read.parse_input("fact: (techgenius profHammond)")
+        print(' Retracting', r1)
+        self.KB.kb_retract(r1)
+        ask1 = read.parse_input("fact: (employable ?X)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertFalse(answer)
+        ask2 = read.parse_input("fact: (smart ?X)")
+        print(' Asking if', ask2)
+        answer = self.KB.kb_ask(ask2)
+        self.assertFalse(answer)
+
+    def test10(self):
+        """this student generated test ensures the inference engine is working at a basic level"""
+        ask1 = read.parse_input("fact: (IronMan ?X)")
+        print(' Asking if', ask1)
+        print("======================facts==========================")
+        for i in self.KB.facts:
+            if i.statement.predicate == "dresslike":
+                print("fact:", i)
+        print("=====================================================")
+        answer = self.KB.kb_ask(ask1)
+        print("answer is: ", answer)
+        self.assertEqual(str(answer[0]), "?X : profHammond")
+        ask2 = read.parse_input("fact: (smart ?X)")
+        print(' Asking if', ask2)
+        answer = self.KB.kb_ask(ask2)
+        self.assertEqual(str(answer[0]), "?X : profHammond")
+        ask3 = read.parse_input("fact: (employable ?X)")
+        print(' Asking if', ask3)
+        answer = self.KB.kb_ask(ask3)
+        self.assertEqual(str(answer[0]), "?X : profHammond")
+        print("--------------------test10---------------------")
 
 def pprint_justification(answer):
     """Pretty prints (hence pprint) justifications for the answer.
--- student_code.py
+++ student_code.py
@@ -115,7 +115,8 @@ class KnowledgeBase(object):
         else:
             print("Invalid ask:", fact.statement)
             return []
-
+    #Implement the `Retract` interface to remove facts from the KB
+    #  - i.e., implement the `KnowledgeBase.kb_retract` method.
     def kb_retract(self, fact_or_rule):
         """Retract a fact from the KB
 
@@ -128,9 +129,58 @@ class KnowledgeBase(object):
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
         # Student code goes here
+
+        if isinstance(fact_or_rule, Fact):
+            if fact_or_rule not in self.facts:
+                #print("fact not in bk!")
+                return
+            else:
+                #find the corresponding fact in kb
+                index = self.facts.index(fact_or_rule)
+                fact_or_rule = self.facts[index]
+                #if the fact is not supported, remove it
+                if len(fact_or_rule.supported_by) == 0:
+                    self.facts.remove(fact_or_rule)
+                else:
+                    #print("can't retract!")
+                    return
+        elif isinstance(fact_or_rule, Rule):
+            if fact_or_rule not in self.rules:
+                #print("rule not in bk!")
+                return
+            else:
+                #find the corresponding rule in kb
+                index = self.rules.index(fact_or_rule)
+                fact_or_rule = self.rules[index]
+                #if rule is not supported and not asserted， then remove it
+                if len(fact_or_rule.supported_by) == 0 and fact_or_rule.asserted != True:
+                    self.rules.remove(fact_or_rule)
+                else:
+                    #print("can't retract!")
+                    return
+        #remove the supported pairs of the facts that it supports
+        for facts in fact_or_rule.supports_facts:
+            for i in facts.supported_by:
+                if fact_or_rule in i:
+                    facts.supported_by.remove(i)
+            if facts.asserted != True:
+                self.kb_retract(facts)
+        #remove the supported pairs of the rules that it supports
+        for rules in fact_or_rule.supports_rules:
+            for i in rules.supported_by:
+                if fact_or_rule in i:
+                    rules.supported_by.remove(i)
+            if rules.asserted != True:
+                self.kb_retract(rules)
+
+
+            
         
 
+
 class InferenceEngine(object):
+    #Implement the forward-chaining inferences that occurs upon asserting facts 
+    # and rules into the KB - i.e., implement the `InferenceEnginer.fc_infer` method.
     def fc_infer(self, fact, rule, kb):
         """Forward-chaining to infer new facts and rules
 
@@ -146,3 +196,29 @@ class InferenceEngine(object):
             [fact.statement, rule.lhs, rule.rhs])
         ####################################################
         # Student code goes here
+        
+        binds = match(fact.statement, rule.lhs[0])
+        if binds != False:
+            #if rule.lhs contains more than one item, a new rule should be inferred
+            if len(rule.lhs) > 1:
+                lhs = []
+                rhs = Statement()
+                for i in rule.lhs[1:]:
+                    lhs.append(instantiate(i, binds))
+                rhs = instantiate(rule.rhs, binds)
+                newrule = Rule([lhs, rhs], [[fact, rule]])
+                fact.supports_rules.append(newrule)
+                rule.supports_rules.append(newrule)
+                kb.kb_assert(newrule)
+            else: 
+                #there is only one item in rule.lhs, so new fact 
+                newfact = Fact(instantiate(rule.rhs, binds), [[fact, rule]])
+                fact.supports_facts.append(newfact)
+                rule.supports_facts.append(newfact)
+                kb.kb_assert(newfact)
+
+        return
+
+
+
+        
